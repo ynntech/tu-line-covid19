ed548431daab276d114ba13447707f7e81f7c6d2
yusuke takahashi <thys_60918@yahoo.co.jp>
2020-04-08 23:09:47 +0900

    pushするユーザを学部指定できるんご

--- linebot/push_message.py
+++ linebot/push_message.py
@@ -2,19 +2,25 @@ from linebot import LineBotApi
 from linebot.models import TextSendMessage
 from linebot.exceptions import LineBotApiError
 import os
+import pandas as pd
 
 LINE_CHANNEL_ACCESS_TOKEN = os.environ["LINE_CHANNEL_ACCESS_TOKEN"]
 line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
 
-def push_message(major, message):
-
+def push_message(message, major):
     """
-    ここでcsvファイルを参照して対象のuseridを取り出す
+    ==Parameters==
+        message(str) : ユーザに送りたいメッセージ 
+        major(str)   : メッセージを送りたい学部（後々学科まで細分化するかも）
+    ==Return==
+        None
     """
+    userid_df = pd.read_csv('user_id.csv', encoding='cp932')
+    target_ids = userid_df.loc[userid_df["department"]==major]["user_id"] #対象学部のuseridのリストを取得
 
-    for userid in userid_list:
+    for userid in target_ids:
         try:                            #メッセージを送信したい相手のIDを入力
-            line_bot_api.push_message(userid,TextSendMessage(text=message))
+            line_bot_api.push_message(userid, TextSendMessage(text=message))
         except LineBotApiError as e:
             print("error!")
 
