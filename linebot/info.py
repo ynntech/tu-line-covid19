b870c962820845f40e7fd1311e4c0df8ee135f39
yusuke takahashi <thys_60918@yahoo.co.jp>
2020-04-09 12:09:36 +0900

    ちょい修正

--- linebot/info.py
+++ linebot/info.py
@@ -14,25 +14,25 @@ info.at_first("工学部")
 
 class Info:
     major_index = {"全体":"tu.pickle",
-                   "文学部":"sal.pickle", "文学研究科":"sal.pickle",
-                   "教育学部":"sed.pickle", "教育学研究科":"sed.pickle",
-                   "法学部":"law.pickle", "法学研究科":"law.pickle",
-                   "経済学部":"econ.pickle", "経済学研究科":"econ.pickle",
-                   "理学部":"sci.pickle", "理学研究科":"sci.pickle",
-                   "医学部":"med.pickle", "医学系研究科":"med.pickle",
-                   "歯学部":"dent.pickle", "歯学研究科":"dent.pickle",
-                   "薬学部":"pharm.pickle", "薬学研究科":"pharm.pickle",
-                   "工学部":"eng.pickle", "工学研究科":"eng.pickle",
-                   "農学部":"agri.pickle", "農学研究科":"agri.pickle",
-                   "国際文化研究科":"intcul.pickle",
-                   "情報科学研究科":"is.pickle",
-                   "生命科学研究科":"lifesci.pickle",
-                   "環境科学研究科":"kankyo.pickle",
-                   "医工学研究科":"bme.pickle"}
+                "文学部":"sal.pickle", "文学研究科":"sal.pickle",
+                "教育学部":"sed.pickle", "教育学研究科":"sed.pickle",
+                "法学部":"law.pickle", "法学研究科":"law.pickle",
+                "経済学部":"econ.pickle", "経済学研究科":"econ.pickle",
+                "理学部":"sci.pickle", "理学研究科":"sci.pickle",
+                "医学部":"med.pickle", "医学系研究科":"med.pickle",
+                "歯学部":"dent.pickle", "歯学研究科":"dent.pickle",
+                "薬学部":"pharm.pickle", "薬学研究科":"pharm.pickle",
+                "工学部":"eng.pickle", "工学研究科":"eng.pickle",
+                "農学部":"agri.pickle", "農学研究科":"agri.pickle",
+                "国際文化研究科":"intcul.pickle",
+                "情報科学研究科":"is.pickle",
+                "生命科学研究科":"lifesci.pickle",
+                "環境科学研究科":"kankyo.pickle",
+                "医工学研究科":"bme.pickle"}
     db_root = os.path.join("..", "sites_db")
 
     def now(self, major):
-        data = self.read(path=major_index[major])
+        data = self.read(path=self.major_index[major])
         if data is None:
             return f"=====\n{major}に登録された情報はありません。"
         else:
