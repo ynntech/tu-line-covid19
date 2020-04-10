1ff76ca4cb28d2ea496c5c438f59748f98d0d6e7
Higuchi Kenichi <ken.tennis.123@gmail.com>
2020-04-11 03:59:50 +0900

    文学部check、db更新プログラム追加

--- superviser/reload.py
+++ superviser/reload.py
@@ -1,30 +1,38 @@
 #-*- coding: utf-8 -*-
-from utils import Superviser
 from sites import (TU, Sal, Sed, Law, Econ, Sci, Med, Dent, Pharm,
                    Eng, Agri, Intcul, IS, Lifesci, Kankyo, Bme)
 
 
 if __name__ == "__main__":
     _tu = TU()
+    _tu.new
     _sal = Sal()
+    _sal.new
     _sed = Sed()
+    _sed.new
     _law = Law()
+    _law.new
     _econ = Econ()
+    _econ.new
     _sci = Sci()
+    _sci.new
     _med = Med()
+    _med.new
     _dent = Dent()
+    _dent.new
     _pharm = Pharm()
+    _pharm.new
     _eng = Eng()
+    _eng.new
     _agri = Agri()
+    _agri.new
     _intcul = Intcul()
+    _intcul.new
     _is = IS()
+    _is.new
     _lifesci = Lifesci()
+    _lifesci.new
     _kankyo = Kankyo()
+    _kankyo.new
     _bme = Bme()
-    targets = [_tu, _sal, _sed, _law, _econ, _sci, _med, _dent, _pharm,
-               _eng, _agri, _intcul, _is, _lifesci, _kankyo, _bme]
-    timers = ["9:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00",
-              "16:00", "17:00", "18:00", "19:00", "20:00","21:00"]
-
-    supervise = Superviser(targets=targets, timers=timers)
-    supervise.run()
+    _bme.new
