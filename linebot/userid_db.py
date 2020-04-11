e0f9cbce93ec840c7b333e45f59baff2d7ae744b
yusuke takahashi <thys_60918@yahoo.co.jp>
2020-04-12 05:13:17 +0900

    DB実装完了

--- linebot/userid_db.py
+++ linebot/userid_db.py
@@ -3,8 +3,8 @@ import psycopg2
 import psycopg2.extras
 import os
 
-# 入力されたSQL文を用いてselectを行い,リストを返却する.
-def get_userid_list(sql):
+# 入力されたSQL文を用いてselectを行い,指定されたユーザー情報のリストを返却する.
+def get_userinfo_list(sql):
     with get_connection() as conn:
         with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as cur:
             cur.execute (sql)
@@ -25,32 +25,32 @@ def get_connection():
 
 # 渡されたuseridをDBから削除する
 def del_userid(userid):
-    sql_delete = "delete from userinfo where userid='{userid}'"
+    sql_delete = f"delete from userinfo where userid='{userid}'"
+    operation_db(sql_delete)
 
-    return "ok"
+# 渡されたユーザ情報をDBに登録する
+def add_userid(department, subject, userid):
+    sql_add = f"insert into userinfo (department, subject, userid) values ('{department}', '{subject}', '{userid}')"
+    operation_db(sql_add)
 
-
-def add_userid(userid):
-    DATABASE_URL = os.environ("DATABASE_URL")
-    engine = create_engine(DATABASE_URL)
-    return "ok"
-
-
-def search_userid(user_major):
-    sql_search = "select * from userinfo where department '%{user_major}%'"
-    target_ids = get_userid_list(sql_search)
-
-
+# 渡された所属のuseridをリストとして返却
+def search_userid(user_major, subject=False):
+    if user_major == "全学生向け":
+        sql_search = "select userid from userinfo" #全行のuseridのリストを取得
+    else:
+        if subject:
+            sql_search = f"select userid from userinfo where subject='{user_major}'" # subject列がuser_majorである行のuseridのリストを取得
+        else:
+            sql_search = f"select userid from userinfo where department='{user_major}'" # department列がuser_majorである行のuseridのリストを取得
+    target_ids = get_userinfo_list(sql_search)
     return target_ids
+
+# 渡されたuseridのdepartmentを返却（subjectには未対応）
+def get_usermajor(userid):
+    sql_search = f"select department from userinfo where userid='{userid}'"
+    user_major = get_userinfo_list(sql_search)
+    return user_major[0][0]
 
 
 if __name__ == "__main__":
-    # 以下を手動実行して最初にテーブルを作成する
-    data = {"department": ["学部"],
-            "subject": ["学科"],
-            "userid": ["1234567890"]}
-    userid_df = pd.DataFrame(data)
-    engine = create_engine("postgres://ohpuvdblvbaowf:df2232602d6697a46a7968aa12ffc67b4bc885fc15b31133839661c71e139cf8@ec2-50-17-21-170.compute-1.amazonaws.com:5432/d3m1hku7dh4kt8")
-    userid_df.to_sql("userinfo", engine, if_exists="replace", index=False)
-
     print("jobs!")
