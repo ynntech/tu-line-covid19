3a93373cb9e4678d5f1c556b92a1f9dfab2c4af9
Yushi Nakaya <38832666+ynntech@users.noreply.github.com>
2020-04-19 20:37:01 +0900

    英語版工学部対応

--- superviser/sites.py
+++ superviser/sites.py
@@ -460,8 +460,7 @@ class Pharm(Site):
         return info_list
 
 
-### 工学部・工学研究科 ###
-class EngNews(News):
+class EngENNews(News):
     def __init__(self, tag, base_url):
         '''
         <parameter>
@@ -474,12 +473,13 @@ class EngNews(News):
     # this should be overrided
     # because the format of news will be different from the others
     def summary(self):
-        time_tag = self.tag.find_next("td")
-        time_split = time_tag.text.split(".")
+        time_tag = self.tag.find_next("th")
+        time_split = time_tag.text.split("/")
         print(f"time_split is {time_split}")
 
-        time = "{}.{}".format(time_split[0], re.search(
-            r'\d+', (time_split[1])).group())
+        # time = "{}/{}/{}".format(time_split[0], re.search(
+        #     r'\d+', (time_split[1])).group())
+        time = "{}/{}/{}".format(time_split[0], time_split[1], time_split[2])
         a_tag = self.tag.find("a")
         if a_tag is not None:
             href = a_tag.get("href")
@@ -487,50 +487,30 @@ class EngNews(News):
                 href = self.base_url + href
             self.content = f"《{time}》\n{a_tag.text}\n{href}"
         else:
-            content = "\n".join(time_tag.find_next("td").text.split())
-            url = "https://www.eng.tohoku.ac.jp/news/detail-,-id,1561.html"
+            content = "\n".join(time_tag.find_next("th").text.split())
+            url = "https://www.eng.tohoku.ac.jp/english/news/news4/"
             self.content = f"《{time}》\n{content}\n{url}"
         self.time = self.timeobj(time)
 
     def timeobj(self, timestr=""):
-        year = "2020."
-        tmp = datetime.datetime.strptime(year + timestr, "%Y.%m.%d")
+        tmp = datetime.datetime.strptime(timestr, "%Y/%m/%d")
         return datetime.date(tmp.year, tmp.month, tmp.day)
 
 
-class Eng(Site):
+class EngEN(Site):
     path = os.path.join("..", os.path.join("sites_db", "eng.pickle"))
-    url = "https://www.eng.tohoku.ac.jp/news/detail-,-id,1561.html"
-    base_url = "https://www.eng.tohoku.ac.jp"
-    majors = ["工学部", "工学研究科"]
+    url = "https://www.eng.tohoku.ac.jp/english/news/news4/"
+    base_url = "https://www.eng.tohoku.ac.jp/english"
+    majors = ["School of Engineering", "Graduate School of Engineering"]
 
     def get(self):
         soup = self.request()
         # 以降、サイトに合わせて書き直す必要あり
-        info_list = soup.find(id="main").find_all("tr")[1:]
+        info_list = soup.find(class_="table nt news").find_all("tr")
+        print(f"info_list is {info_list}")
         info_list = self.abstract(info_list)
 
-        # 固定情報
-        stick1 = EngNews(info_list[0], self.base_url)
-        contents = ["《4/10》",
-                    "【新型コロナウイルス感染拡大防止のための自宅待機のお願い】",
-                    "令和2年4月9日(木)から5月6日(水)まで、原則として登校を禁止し、研究室活動を制限します",
-                    "https://www.eng.tohoku.ac.jp/news/detail-,-id,1582.html",
-                    "https://www.eng.tohoku.ac.jp/news/detail-,-id,1581.html"]
-        stick1.time = stick1.timeobj(timestr="4.10")
-        stick1.content = "\n".join(contents)
-
-        stick2 = EngNews(info_list[0], self.base_url)
-        contents = ["《4/10》",
-                    "【全学生 要回答】東北大ID受取確認(新入生対象), 遠隔授業の受講環境等の調査を実施しています。",
-                    "https://www.eng.tohoku.ac.jp/news/detail-,-id,1576.html#survey"]
-        stick2.time = stick1.timeobj(timestr="4.10")
-        stick2.content = "\n".join(contents)
-        sticks = [stick1, stick2]
-        ###
-
-        info_list = [EngNews(info, self.base_url) for info in info_list]
-        info_list = sticks + info_list
+        info_list = [EngENNews(info, self.base_url) for info in info_list]
         return self.dic(info_list)
 
     def abstract(self, tags=[]):
@@ -541,43 +521,6 @@ class Eng(Site):
                 result.append(tag)
                 exception.append(tag.text)
         return result
-
-# 工学研究科英語
-
-
-# class EngENNews(News):
-#     # this should be overrided
-#     # because the format of news will be different from the others
-#     def summary(self):
-#         time = self.tag.find("th").text
-#         a_tags = self.tag.find_all("a")
-#         contents = []
-#         links = []
-#         for i in range(len(a_tags)):
-#             href = a_tags[i].get("href")
-#             links.append(href)
-#             contents.append(a_tags[i].text)
-#         contents = "\n".join(contents)
-#         links = "\n".join(links)
-#         self.content = f"《{time}》\n{contents}\n{links}"
-#         self.time = self.timeobj(time)
-
-#     def timeobj(self, timestr=""):
-#         tmp = datetime.datetime.strptime(timestr, "%Y年%m月%d日")
-#         return datetime.date(tmp.year, tmp.month, tmp.day)
-
-
-# class EngEN(Site):
-#     path = os.path.join("..", os.path.join("sites_db", "intcul.pickle"))
-#     url = "http://www.intcul.tohoku.ac.jp/"
-#     majors = ["国際文化研究科"]
-
-#     def get(self):
-#         soup = self.request()
-#         # 以降、サイトに合わせて書き直す必要あり
-#         info_list = soup.find_all("tr")
-#         info_list = [EngENNews(info) for info in info_list]
-#         return self.dic(info_list)
 
 
 ### 農学部・農学研究科 ###
